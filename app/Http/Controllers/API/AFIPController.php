3a7ac8d826cc6735b6839c50a79ead8a42e1c28b
cesar2016 <cesars.pro@gmail.com>
2022-01-26 17:34:08 -0300

    poniendo impTotCom

--- app/Http/Controllers/API/AFIPController.php
+++ app/Http/Controllers/API/AFIPController.php
@@ -156,8 +156,8 @@ class AFIPController extends Controller
             'CbteHasta' => $valfac,  // Número de comprobante o numero del último comprobante en caso de ser mas de uno *
             'CbteFch' 	=> intval(date('Ymd')),//$date_cte, //intval(date('Ymd')), (Opcional) Fecha del comprobante (yyyymmdd) o fecha actual si es nulo *
             'ImpTotal' 	=> 100, //$request->total_amount, // Importe total del comprobante * 
-            /* 'ImpTotConc'=> 0,            
-            'ImpNeto' => 100, //$request->total_amount, // Importe neto gravado, al colocar este valor > que  0 tienen que exitir tambien IVA
+            'ImpTotConc'=> 0,            
+            /*'ImpNeto' => 100, //$request->total_amount, // Importe neto gravado, al colocar este valor > que  0 tienen que exitir tambien IVA
             'ImpOpEx' 	=> 0,   // Importe exento de IVA
             'ImpIVA' 	=> 0,  //Importe total de IVA
             'ImpTrib' 	=> 0,   //Importe total de tributos */
